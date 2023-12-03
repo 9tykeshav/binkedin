17e579b251384e2ff4c5f432450b5216a5a9ff3d
9tykeshav <57292513+9tykeshav@users.noreply.github.com>
2023-12-03 16:46:01 +0530

    added register endpoint

--- binkedin/src/httproutes/onboarding/login.rs
+++ binkedin/src/httproutes/onboarding/login.rs
@@ -1,18 +1,18 @@
 use axum::{
+    debug_handler,
     extract::State,
     http::StatusCode,
     routing::{get, post},
-    Json, Router,
+    Error, Json, Router,
 };
 use serde::{Deserialize, Serialize};
-use sqlx::query_as;
+use sqlx::{error, Row};
 use sqlx::{
     pool,
     postgres::{PgPool, PgPoolOptions},
     query,
 };
-
-use sqlx::Row;
+use sqlx::{postgres::PgQueryResult, query_as};
 use std::time::Duration;
 
 use crate::Ctx;
@@ -20,6 +20,7 @@ use crate::Ctx;
 pub fn router<S>(state: crate::Ctx) -> Router<S> {
     let router = Router::new()
         .route("/login", get(login_handler))
+        .route("/register", post(register))
         .with_state(state);
     router
 }
@@ -42,12 +43,67 @@ async fn login_handler(
     });
 
     let user = User {
-        email: data.email.clone().unwrap_or("NULL".to_string()),
-        password: data.password.unwrap_or("NULL".to_string()),
         does_exits: if data.email == None { false } else { true },
+        email: data.email.unwrap_or("NULL".to_string()),
+        password: data.password.unwrap_or("NULL".to_string()),
     };
-    (StatusCode::OK, Json(user))
+    //TODO: match and return 404 for not existing users
+    let status = if user.does_exits {
+        StatusCode::OK
+    } else {
+        StatusCode::NOT_FOUND
+    };
+    (status, Json(user))
 }
+
+#[debug_handler]
+async fn register(
+    State(ctx): State<crate::Ctx>,
+    Json(payload): Json<Login_data>,
+) -> (StatusCode, Json<Register_user_data>) {
+    let query = sqlx::query!(
+        "INSERT INTO users VALUES($1, $2)",
+        payload.email,
+        payload.password
+    )
+    .execute(&ctx.db)
+    .await;
+    match query {
+        Ok(PgQueryResult) => (
+            StatusCode::CREATED,
+            Json(Register_user_data {
+                email: payload.email,
+                password: payload.password,
+                info: "SUCCESS".to_string(),
+            }),
+        ),
+        Err(Error) => {
+            let errr = Error.as_database_error();
+            match errr.unwrap().kind() {
+                sqlx::error::ErrorKind::UniqueViolation => (
+                    StatusCode::CONFLICT,
+                    Json(Register_user_data {
+                        email: payload.email,
+                        password: payload.password,
+                        info: "USER ALREADY EXISTS".to_string(),
+                    }),
+                ),
+                _ => {
+                    println!("{}", errr.unwrap());
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json(Register_user_data {
+                            email: payload.email,
+                            password: payload.password,
+                            info: "INTERNAL SERVER ERROR ".to_string(),
+                        }),
+                    )
+                }
+            }
+        }
+    }
+}
+
 // hello
 #[derive(Serialize)]
 struct User {
@@ -55,7 +111,7 @@ struct User {
     password: String,
     does_exits: bool,
 }
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct Login_data {
     email: String,
     password: String,
@@ -65,4 +121,11 @@ struct Login_data {
 struct db_data {
     email: Option<String>,
     password: Option<String>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Register_user_data {
+    email: String,
+    password: String,
+    info: String,
 }
